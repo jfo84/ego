f07a8768e7cbaf355e652b298096cf91d477fe58
Jack Ford <jford8820@gmail.com>
2018-03-07 13:13:18 -0600

    Initial styling done for header. Much more CSS to be done before worrying about a JS widget

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin-top: 0;
+  z-index: 1;
+  overflow: hidden;
+  background: #0003d1;
+`;
+
+const Header = styled.section`
+  margin: auto;
+  max-width: 760px;
+`;
+
+const Title = styled.h1`
+  font-family: "Mina", sans-serif;
+  font-weight: 300;
+  font-size: 1.5em;
+  text-align: left;
+  color: #FDFDFD;
+  margin-bottom: 0.15em;
+`;
+
+const Description = styled.h2`
+  font-family: "Muli", sans-serif;
+  font-size: 1.5em;
+  text-align: left;
+  color: #FDFDFD;
+  margin-top: 0;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <head>
+          <meta charSet="utf-8" />
+          <title>Jack Ford</title>
+
+          <link rel="canonical" href="http://www.jruffinford.com" />
+          <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Mina|Muli" />
+
+          <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        </head>
+
+        <Wrapper>
+          <Header>
+            <Title>Jack Ford</Title>
+            <Description>Software Engineer</Description>
+          </Header>
+        </Wrapper>
       </div>
     );
   }
