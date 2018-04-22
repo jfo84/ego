d3ffaa12a79745435cbfc634982b00847ffcf02d
Jack Ford <jford8820@gmail.com>
2018-04-22 13:57:05 +0200

    Add FlexBox utility components

---
+++ src/components/Flex.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FlexRow = styled.div`
+  display: flex;
+  direction: row;
+`;
+
+export const FlexStart = styled.div`
+  flex: 1;
+  justify-content: "flex-start";
+`;
+
+export const FlexEnd = styled.div`
+  flex: 1;
+  justify-content: "flex-start";
+`;
